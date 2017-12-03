b0c7c81c230eaa14d6d1f4a15f32b9f9a06b0089
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-12-02 23:14:40 -0600

    pseudocode

---
+++ greatest.rb
@@ -0,0 +1,8 @@
+# input array of integers
+# output a single integer that is the greatest value in the array
+
+# loop through the array
+# compare each value to the previous value
+  # store whichever value is greater and compare that to the next value
+# return the stored value
+
